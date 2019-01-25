0f6ce13d80f56a80717089e8e742a29d559c91f8
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-25 11:46:39 -0800

    add routing for 5 component types

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,39 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 // Import Components to add to the router.
-import { ExampleComponent } from './example/example.component';
+import { ExampleComponent } from "./example/example.component";
+import { AboutComponent } from "./about/about.component";
+import { RadarComponent } from "./radar/radar.component";
+import { ReportageComponent } from "./reportage/reportage.component";
+import { ShopComponent } from "./shop/shop.component";
+import { VideosComponent } from "./videos/videos.component";
 
-
-const appRoutes: Routes = [ 
-    //An example route 
-    {
-        path: '',
-        component: ExampleComponent
-    }
+const appRoutes: Routes = [
+  //An example route
+  {
+    path: "",
+    component: ExampleComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "radar",
+    component: RadarComponent
+  },
+  {
+    path: "reportage",
+    component: ReportageComponent
+  },
+  {
+    path: "shop",
+    component: ShopComponent
+  },
+  {
+    path: "videos",
+    component: VideosComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
