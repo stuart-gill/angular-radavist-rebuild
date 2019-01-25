6f0e1738b3d3c3cb6cf764ece7b3adc559d3dd87
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-25 09:58:28 -0800

    create 5 primary components and update index title and image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { AboutComponent } from './about/about.component';
+import { ReportageComponent } from './reportage/reportage.component';
+import { RadarComponent } from './radar/radar.component';
+import { VideosComponent } from './videos/videos.component';
+import { ShopComponent } from './shop/shop.component';
 
 
 //Add new Components to the declarations to initialize and add a component.
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    AboutComponent,
+    ReportageComponent,
+    RadarComponent,
+    VideosComponent,
+    ShopComponent
   ],
 
   // Add modules to the imports.
---
+++ src/app/reportage/reportage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportage',
+  templateUrl: './reportage.component.html',
+  styleUrls: ['./reportage.component.css']
+})
+export class ReportageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
