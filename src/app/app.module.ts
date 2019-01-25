cc8cbeb5fa710b140912981ba25e91bb5e1e719d
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-25 11:30:06 -0800

    attempting to add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+//import { HttpModule } from '@angular/http';
+import { routing } from "./app.routing";
+//import { masterFirebaseConfig } from './api-keys';
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
+//import { AngularFireModule } from 'angularfire2';
+import { AppComponent } from "./app.component";
+import { ExampleComponent } from "./example/example.component";
+import { AboutComponent } from "./about/about.component";
+import { ReportageComponent } from "./reportage/reportage.component";
+import { RadarComponent } from "./radar/radar.component";
+import { VideosComponent } from "./videos/videos.component";
+import { ShopComponent } from "./shop/shop.component";
 
-import { routing } from './app.routing';
-
-import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
-import { AboutComponent } from './about/about.component';
-import { ReportageComponent } from './reportage/reportage.component';
-import { RadarComponent } from './radar/radar.component';
-import { VideosComponent } from './videos/videos.component';
-import { ShopComponent } from './shop/shop.component';
-
-
+// export const firebaseConfig = {
+//   apiKey: masterFirebaseConfig.apiKey,
+//   authDomain: masterFirebaseConfig.authDomain,
+//   databaseURL: masterFirebaseConfig.databaseURL,
+//   storageBucket: masterFirebaseConfig.storageBucket
+// };
 //Add new Components to the declarations to initialize and add a component.
 @NgModule({
   declarations: [
@@ -27,9 +35,13 @@ import { ShopComponent } from './shop/shop.component';
   // Add modules to the imports.
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
+    //HttpModule,
+    //AngularFireModule.initializeApp(firebaseConfig),
+    //AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
